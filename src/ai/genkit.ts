00fa2c6464dafa3aa37de4700c8a081a1ec9acbc
FahadBinAbdullah7 <78896691+FahadBinAbdullah7@users.noreply.github.com>
2025-09-12 11:35:28 +0600

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,8 +4,7 @@ import {googleAI} from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey:
-        process.env.GOOGLE_API_KEY || 'AIzaSyCWFqfCAdrqAgQFB1JKpoLadvV9QGzw14E',
+      apiKey: process.env.GOOGLE_API_KEY,
     }),
   ],
   model: 'googleai/gemini-2.5-flash',
